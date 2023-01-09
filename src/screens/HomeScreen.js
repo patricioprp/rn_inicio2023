b8e05d98d93d127f64e53d5b77759de2ce0981cc
Patricio <patricioprp06@gmail.com>
2023-01-09 14:02:19 -0300

    se agrega TextScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -31,6 +31,11 @@ const HomeScreen = ({navigation}) => {
     />
 
 <Button 
+    onPress={()=> navigation.navigate('Text')}
+    title = "Go to Text Demo"
+    />
+
+<Button 
     onPress={()=> navigation.navigate('Square')}
     title = "Go to Square Demo"
     />
---
+++ src/screens/TextScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Text,View, StyleSheet} from 'react-native'
+
+const TextScreen  = () => {
+    return<View>
+        <Text>Text Screen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default TextScreen;
+
