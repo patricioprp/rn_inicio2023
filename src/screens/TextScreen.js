06975608b9bb1df3c2d7388e30c17cdf46fec643
Patricio <patricioprp06@gmail.com>
2023-01-10 15:56:47 -0300

    se termina la patalla de texto

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import {Text,View, StyleSheet} from 'react-native'
+import React, {useState} from 'react'
+import {Text,View, StyleSheet, TextInput} from 'react-native'
 
 const TextScreen  = () => {
+    const [name, setName] = useState('');
     return<View>
-        <Text>Text Screen</Text>
+        <Text>Ingrese su nombre</Text>
+        <TextInput 
+        style={styles.input}
+        autoCapitalize = 'none'
+        autoCorrect={false}
+        value={name}
+        onChangeText={newValue => setName(newValue)}
+        />
+        <Text>Su nombre es {name}</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
-
+    input:{
+        margin: 15,
+        borderColor: 'red',
+        borderWidth: 1
+    }
 });
 
 export default TextScreen;
