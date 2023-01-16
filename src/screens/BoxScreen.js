7ec19c737ae41acce677b953737e0b5783ad7f6e
Patricio <patricioprp06@gmail.com>
2023-01-16 01:26:54 -0300

    se trabaja con flex box

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,19 +3,37 @@ import { Text, View, StyleSheet } from "react-native";
 
 const BoxScreeen = () => {
     return <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Text BoxScreeen</Text>
+        <Text style={styles.textOneStyle}>Hijo #1</Text>
+        <Text style={styles.textTwoStyle}>Hijo #2</Text>
+        <Text style={styles.textThreeStyle}>Hijo #3</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
     viewStyle:{
-        borderWidth:10,
-        borderColor:'black'
+        borderWidth:3,
+        borderColor:'black',
+        height:200 ,
+        //flexDirection: 'row',
+        // alignItems: 'flex-end',
+        // justifyContent: 'flex-end'
     },
-    textStyle:{
-        borderWidth: 1,
+    textOneStyle:{
+        borderWidth: 3,
         borderColor: 'red',
-        margin: 20
+        flex: 4
+
+    },
+    textTwoStyle:{
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 4
+
+    },
+    textThreeStyle:{
+        borderWidth: 3,
+        borderColor: 'red',
+        flex: 2
 
     }
 });
